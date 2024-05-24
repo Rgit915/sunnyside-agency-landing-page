70b29f2e39faf67cf9220911db8390e0c6ceb1b1
Rgit915 <rora.goitom@gmail.com>
2024-05-24 13:25:33 -0400

    Implement toggle menu for small screens

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,33 +1,60 @@
+import { useState } from "react";
+
 import logo from "../assets/images/logo.svg";
 import humburgerMenu from "../assets/images/icon-hamburger.svg";
 
 const NavLinks = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleOpenMenu = () => {
+    setOpen(!isOpen);
+  };
   return (
     <>
-      <nav>
-        <div className="flex justify-between items-center ">
+      <nav className="">
+        <div className=" flex justify-between items-center ">
           <div className="logo">
             <img src={logo} alt="Sunnyside logo" />
           </div>
           <div>
-            <img src={humburgerMenu} alt="Humburger Menu" />
+
+            <ul className="nav-links hidden w-full md:w-1/3 md:flex md:flex-row md:space-x-6">
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#services">Services</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#contacts">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className="md:hidden">
+            <button type="button" onClick={handleOpenMenu}>
+              <img src={humburgerMenu} alt="Humburger Menu" />
+            </button>
           </div>
         </div>
-
-        <ul className="hidden">
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#services">Services</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contacts">Contact</a>
-          </li>
-        </ul>
+        {isOpen && (
+          <ul className="menu bg-white font-barlow font-semibold text-[20px] text-grayish-blue leading-6 tracking-[-0.14px] p-4 z-10 flex flex-col justify-around items-center transition ease-in-out duration-300 absolute right-6 mt-8 space-y-4 md:hidden">
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contacts">Contact</a>
+            </li>
+          </ul>
+        )}
       </nav>
     </>
   );
